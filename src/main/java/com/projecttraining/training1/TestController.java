9cdc1fe4b852d40006c4990069ace510b767d0b6
Cipriano Cruz Jr <ciprianocruzjr01@gmail.com>
2023-10-25 10:22:20 +0800

    first commit

---
+++ src/main/java/com/projecttraining/training1/TestController.java
@@ -0,0 +1,13 @@
+package com.projecttraining.training1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
